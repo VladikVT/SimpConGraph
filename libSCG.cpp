188bd48f1851305cc786247f2a0eca6bfc02e798
VladikVT <vladislavtimirbaev@gmail.com>
2022-04-17 16:02:24 +0500

    Small changes in libSCG.cpp

--- libSCG.cpp
+++ libSCG.cpp
@@ -281,7 +281,7 @@ void libSCG::circle(string symbol, int x, int y, int radius, bool fill)
 		}
 	}
 }
-
+// The Bresenham`s line algorithm is used
 void libSCG::line(string symbol, int x1, int y1, int x2, int y2)
 {
 	if (x1 < 0 || y1 < 0 || x2 >= sizeX || y2 >= sizeY)
